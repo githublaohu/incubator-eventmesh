ab406aacb89b3ae1bd68ba3bd9346711e4fb9d9a
Allen Huang <a11en.huang@foxmail.com>
2022-10-07 19:36:39 +0800

    add AbstractStorageSQLTest

--- eventmesh-connector-plugin/eventmesh-connector-storage-jdbc/src/main/java/org/apache/eventesh/connector/storage/jdbc/SQL/AbstractStorageSQL.java
+++ eventmesh-connector-plugin/eventmesh-connector-storage-jdbc/src/main/java/org/apache/eventesh/connector/storage/jdbc/SQL/AbstractStorageSQL.java
@@ -4,83 +4,90 @@ import lombok.Setter;
 
 public abstract class AbstractStorageSQL implements StorageSQL {
 
-	@Setter
-	protected String offsetField;
+    @Setter
+    protected String offsetField;
 
-	protected String idFieldName = "cloud_event_info_id";
+    protected String idFieldName = "cloud_event_info_id";
 
-	protected String timeFieldName = "cloud_event_create_time";
+    protected String timeFieldName = "cloud_event_create_time";
 
-	protected String insertSQL = "";
+    protected String insertSQL = "";
 
-	protected String selectSQL = "";
+    protected String selectSQL = "";
 
-	protected String updateOffset = "";
+    protected String updateOffset = "";
 
-	@Override
-	public String insertSQL(String tableName) {
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append("insert info ").append(tableName).append(
-				"(cloud_event_id,cloud_event_topic,cloud_event_storage_node_adress,cloud_event_type,cloud_event_producer_group_name,cloud_event_source,cloud_event_content_type,cloud_event_tag,cloud_event_extensions,cloud_event_data)")
-				.append("values(?,?,?,?,?,?,?,CAST(? as JSON),?,?)");
-		return stringBuffer.toString();
-	}
+    @Override
+    public String insertSQL(String tableName) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("INSERT INFO ")
+                .append(tableName)
+                .append("(cloud_event_id,cloud_event_topic,cloud_event_storage_node_adress,cloud_event_type,cloud_event_producer_group_name,cloud_event_source,cloud_event_content_type,cloud_event_tag,cloud_event_extensions,cloud_event_data) ")
+                .append("VALUES(?,?,?,?,?,?,?,CAST(? as JSON),?,?)");
+        return stringBuffer.toString();
+    }
 
-	@Override
-	public String selectSQL(String tableName) {
-		return this.queryLocationEventSQL(tableName);
-	}
+    @Override
+    public String selectSQL(String tableName) {
+        return this.queryLocationEventSQL(tableName);
+    }
 
-	public String selectConsumerGroup() {
-		return "select * from consumer_group_info";
-	}
+    public String selectConsumerGroup() {
+        return "SELECT * FROM consumer_group_info";
+    }
 
-	public String insertConsumerGroup() {
-		return null;
-	}
+    public String insertConsumerGroup() {
+        return null;
+    }
 
-	public String locationEventSQL(String tableName) {
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append("update ").append(tableName).append(" set json_set( cloud_event_consume_location , ? ,? )")
-				.append(" where cloud_event_info_id > ? and json_extract(cloud_event_consume_location, ?) is null limit 200");
-		return stringBuffer.toString();
-	}
+    public String locationEventSQL(String tableName) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("UPDATE ")
+                .append(tableName)
+                .append(" SET json_set( cloud_event_consume_location , ? ,? )")
+                .append(" WHERE cloud_event_info_id > ? AND json_extract(cloud_event_consume_location, ?) IS NULL LIMIT 200");
+        return stringBuffer.toString();
+    }
 
-	public String queryLocationEventSQL(String tableName) {
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append("select * from ").append(tableName).append(" where cloud_event_info_id > ? and JSON_CONTAINS_PATH(cloud_event_consume_location, 'one', ?)");
-		return stringBuffer.toString();
-	}
+    public String queryLocationEventSQL(String tableName) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("SELECT * FROM ").append(tableName).append(" WHERE cloud_event_info_id > ? AND JSON_CONTAINS_PATH(cloud_event_consume_location, 'one', ?)");
+        return stringBuffer.toString();
+    }
 
-	private StringBuffer getSelectSQLById(String tableName) {
-		StringBuffer sql = new StringBuffer();
-		return sql.append("select '").append(tableName).append("' as tableName , ").append(idFieldName).append(" from ")
-				.append(tableName);
-	}
+    private StringBuffer getSelectSQLById(String tableName) {
+        StringBuffer sql = new StringBuffer();
+        return sql.append("SELECT '").append(tableName).append("' AS tableName, ").append(idFieldName).append(" FROM ")
+                .append(tableName);
+    }
 
-	// select * from tables where id > {id} and
-	public String selectFastMessageSQL(String tableName) {
-		return this.getSelectSQLById(tableName).append("  order by  ").append(idFieldName).append(" limit 1").toString();
-	}
+    // select * from tables where id > {id} and
+    public String selectFirstMessageSQL(String tableName) {
+        return this.getSelectSQLById(tableName).append(" ORDER BY ").append(idFieldName).append(" LIMIT 1").toString();
+    }
 
-	public String selectLastMessageSQL(String tableName) {
-		return this.getSelectSQLById(tableName).append("  order by  ").append(idFieldName).append(" desc  limit 1").toString();
-	}
+    public String selectLastMessageSQL(String tableName) {
+        return this.getSelectSQLById(tableName).append(" ORDER BY ").append(idFieldName).append(" DESC LIMIT 1").toString();
+    }
 
-	public String selectNoConsumptionMessageSQL(String tableName, Long consumerGroupId) {
-		return this.getSelectSQLById(tableName).append(" where ").append(timeFieldName).append("> ? and json_extract(cloud_event_consume_location, ?) is not null limit 1 ")
-				.append(offsetField).append(" & ").append(consumerGroupId).append(" = ").append(consumerGroupId)
-				.toString();
-	}
+    public String selectNoConsumptionMessageSQL(String tableName, Long consumerGroupId) {
+        return this.getSelectSQLById(tableName)
+                .append(" WHERE ")
+                .append(timeFieldName).append(" > ? AND json_extract(cloud_event_consume_location, ?) IS NOT NULL LIMIT 1 ")
+                .append(offsetField).append(" & ")
+                .append(consumerGroupId).append(" = ").append(consumerGroupId)
+                .toString();
+    }
 
-	public String selectAppointTimeMessageSQL(String tableName, String time) {
-		return this.getSelectSQLById(tableName).append(" where ").append(timeFieldName).append(" > ? limit 1")
-				.toString();
-	}
+    public String selectAppointTimeMessageSQL(String tableName, String time) {
+        return this.getSelectSQLById(tableName)
+                .append(" WHERE ").append(timeFieldName).append(" > ? LIMIT 1")
+                .toString();
+    }
 
-	@Override
-	public String createDatabaseSQL(String batabase) {
-		return "create database if not exists " + batabase;
-	}
+    @Override
+    public String createDatabaseSQL(String database) {
+        return String.join(" ", "CREATE DATABASE IF NOT EXISTS", database);
+    }
 
 }
--- eventmesh-connector-plugin/eventmesh-connector-storage-jdbc/src/test/java/org/apache/eventesh/connector/storage/jdbc/SQL/AbstractStorageSQLTest.java
+++ eventmesh-connector-plugin/eventmesh-connector-storage-jdbc/src/test/java/org/apache/eventesh/connector/storage/jdbc/SQL/AbstractStorageSQLTest.java
@@ -5,29 +5,44 @@ import org.junit.Test;
 
 public class AbstractStorageSQLTest {
 
-	MySQLStorageSQL storageSQL = new MySQLStorageSQL();
-	
-	@Test
-	public void testLocationEventSQL() {
-		String sql = storageSQL.locationEventSQL(StorageSQLTest.TABLE_NAME);
-		Assert.assertEquals(sql, "update cloud_event_test set json_set( cloud_event_consume_location , ? ,? ) where cloud_event_info_id > ? and json_extract(cloud_event_consume_location, ?) is null limit 200");
-	}
-	
-	@Test
-	public void testQueryLocationEventSQL() {
-		String sql = storageSQL.queryLocationEventSQL(StorageSQLTest.TABLE_NAME);
-		Assert.assertEquals(sql, "select * from cloud_event_test where cloud_event_info_id > ? and JSON_CONTAINS_PATH(cloud_event_consume_location, 'one', ?)");
-	}
-	
-	@Test
-	public void testSelectFastMessageSQL() {
-		String sql = storageSQL.selectFastMessageSQL(StorageSQLTest.TABLE_NAME);
-		Assert.assertEquals(sql, "select 'cloud_event_test' as tableName , cloud_event_info_id from cloud_event_test  order by  cloud_event_info_id limit 1");
-	}
-	
-	@Test
-	public void testSelectLastMessageSQL() {
-		String sql = storageSQL.selectLastMessageSQL(StorageSQLTest.TABLE_NAME);
-		Assert.assertEquals(sql, "select 'cloud_event_test' as tableName , cloud_event_info_id from cloud_event_test  order by  cloud_event_info_id desc  limit 1");
-	}
+    public static final String DATABASE_NAME = "database";
+
+    MySQLStorageSQL storageSQL = new MySQLStorageSQL();
+
+    @Test
+    public void insertSQLTest() {
+        String sql = storageSQL.insertSQL(StorageSQLTest.TABLE_NAME);
+        Assert.assertEquals(sql, "INSERT INFO cloud_event_test(cloud_event_id,cloud_event_topic,cloud_event_storage_node_adress,cloud_event_type,cloud_event_producer_group_name,cloud_event_source,cloud_event_content_type,cloud_event_tag,cloud_event_extensions,cloud_event_data) VALUES(?,?,?,?,?,?,?,CAST(? as JSON),?,?)");
+    }
+
+    @Test
+    public void locationEventSQLTest() {
+        String sql = storageSQL.locationEventSQL(StorageSQLTest.TABLE_NAME);
+        Assert.assertEquals(sql, "UPDATE cloud_event_test SET json_set( cloud_event_consume_location , ? ,? ) WHERE cloud_event_info_id > ? AND json_extract(cloud_event_consume_location, ?) IS NULL LIMIT 200");
+    }
+
+    @Test
+    public void queryLocationEventSQLTest() {
+        String sql = storageSQL.queryLocationEventSQL(StorageSQLTest.TABLE_NAME);
+        Assert.assertEquals(sql, "SELECT * FROM cloud_event_test WHERE cloud_event_info_id > ? AND JSON_CONTAINS_PATH(cloud_event_consume_location, 'one', ?)");
+    }
+
+    @Test
+    public void selectFirstMessageSQLTest() {
+        String sql = storageSQL.selectFirstMessageSQL(StorageSQLTest.TABLE_NAME);
+        Assert.assertEquals(sql, "SELECT 'cloud_event_test' AS tableName, cloud_event_info_id FROM cloud_event_test ORDER BY cloud_event_info_id LIMIT 1");
+    }
+
+    @Test
+    public void selectLastMessageSQLTest() {
+        String sql = storageSQL.selectLastMessageSQL(StorageSQLTest.TABLE_NAME);
+        Assert.assertEquals(sql, "SELECT 'cloud_event_test' AS tableName, cloud_event_info_id FROM cloud_event_test ORDER BY cloud_event_info_id DESC LIMIT 1");
+    }
+
+    @Test
+    public void createDatabaseSQLTest() {
+        String sql = storageSQL.createDatabaseSQL(DATABASE_NAME);
+        Assert.assertEquals(sql, "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME);
+    }
+
 }
